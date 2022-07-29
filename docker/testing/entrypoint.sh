75eca5e9b3f825d09da269af052fa0342a4dc649
Artur Barseghyan <artur.barseghyan@gmail.com>
2022-07-30 01:38:03 +0200

    More

--- docker/testing/entrypoint.sh
+++ docker/testing/entrypoint.sh
@@ -4,8 +4,8 @@ cd /code/
 
 # Pip install requirements
 echo "Testing Elasticsearch 7.x: Installing requirements"
-pip-compile requirements/testing.in
-pip install -r requirements/testing.txt
+pip-compile requirements/testing_7x.in
+pip install -r requirements/testing_7x.txt
 
 # Clean up
 echo "Testing Elasticsearch 7.x: Clean up"
